4f326b54491e5baf9e85a40ec2b7f587f51856a2
Arpan Pandey <pandeyarpan2006@gmail.com>
2025-09-14 18:25:05 +0530

    Add files via upload

--- Code031.c
+++ Code031.c
@@ -3,12 +3,28 @@
 #include <stdio.h>
 
 int main() {
-    int num,i;
-    int bin[100];
+    int num, binary[32], i = 0;
 
-    printf("Enter your number : ");
-    scanf("%d" , &num);
+    printf("Enter a number: ");
+    scanf("%d", &num);
 
-    for (i = num%2; i <= num; i++ )
+    if (num == 0) {
+        printf("Binary: 0\n");
+        return 0;
+    }
+
+    while (num > 0) {
+        binary[i] = num % 2;   
+        num = num / 2;         
+        i++;
+    }
+
+    printf("Binary: ");
+
+    for (i = i - 1; i >= 0; i--) {
+        printf("%d", binary[i]);
+    }
+    printf("\n");
+
     return 0;
 }
