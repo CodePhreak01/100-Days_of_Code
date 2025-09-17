f91246a07c4cf7718274e0929b2b06b13d1a4393
Arpan Pandey <pandeyarpan2006@gmail.com>
2025-09-17 23:23:21 +0530

    Add files via upload

---
+++ Code049.c
@@ -0,0 +1,20 @@
+// Write a program to print the following pattern :
+// 5
+// 45
+// 345
+// 2345
+// 12345
+
+#include <stdio.h>
+
+int main() {
+    int num = 5,count,i,j;
+    
+    for (i = 1; i <= num; i++) {
+        for (j = num - i + 1; j <= num; j++) { 
+            printf(" %d" , j);
+        }
+        printf("\n");
+    }        
+    return 0;
+}
