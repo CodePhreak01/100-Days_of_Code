7dec0516b9bf70f20bcd3bc004eef839b50d0c2d
Arpan Pandey <pandeyarpan2006@gmail.com>
2025-09-22 19:05:36 +0530

    Add files via upload

---
+++ Code053.c
@@ -0,0 +1,32 @@
+// Write a program to print the following pattern:
+// *
+// ***
+// *****
+// *******
+// *********
+// *******
+// *****
+// ***
+// *
+
+#include <stdio.h>
+
+int main() {
+    int n = 5,i,j;
+    
+    for (i = 1; i <= n; i++) {
+        int stars = 2 * i - 1;
+        for (j = 1; j <= stars; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for ( i = n - 1; i >= 1; i--) {
+        int stars = 2 * i - 1;
+        for (j = 1; j <= stars; j++) {
+            printf("*");
+        }
+        printf("\n");      
+    }
+    return 0;
+} 
