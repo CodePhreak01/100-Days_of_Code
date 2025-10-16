49a55e39db47f8d1cf0d51f15e8f4ca3578aa7dd
Arpan Pandey <pandeyarpan2006@gmail.com>
2025-10-16 20:01:49 +0530

    Add files via upload

---
+++ Code074.c
@@ -0,0 +1,34 @@
+// Q74: Find the transpose of a matrix.
+
+#include <stdio.h>
+
+int main() {
+    int rows,col;
+    int i,j;
+    
+    printf("Enter rows and columns respectively : ");
+    scanf("%d %d" , &rows, &col);
+    int mat[rows][col];
+
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < col; j++) {
+            printf("Enter element (%d,%d): ", i,j);
+            scanf("%d" , &mat[i][j]);
+        }
+    }
+    int transpose[i][j];
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < col; j++) {
+            transpose[j][i] = mat[i][j];
+        }
+    }
+    printf("Transposed Matrix : \n");
+    for (i = 0; i < col; i++) {
+        for (j = 0; j < rows; j++) {
+            printf("%d " , transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}   
